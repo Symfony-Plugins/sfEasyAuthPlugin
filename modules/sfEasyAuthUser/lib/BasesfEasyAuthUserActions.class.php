de57ab99825f01f57452b0430cf9afd47e5e7056
allyb <allyb@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-10-27 11:31:56 -0500

    Modified the processForm method to fix a redirection bug where symfony was generating invalid urls
    git-svn-id: http://svn.symfony-project.com/plugins/sfEasyAuthPlugin/trunk@23393 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfEasyAuthUser/lib/BasesfEasyAuthUserActions.class.php
+++ modules/sfEasyAuthUser/lib/BasesfEasyAuthUserActions.class.php
@@ -13,4 +13,33 @@ require_once dirname(__FILE__).'/sfEasyAuthUserGeneratorHelper.class.php';
  */
 class BasesfEasyAuthUserActions extends autoSfEasyAuthUserActions
 {
+  protected function processForm(sfWebRequest $request, sfForm $form)
+  {
+    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
+    if ($form->isValid())
+    {
+      $notice = $form->getObject()->isNew() ? 'The item was created successfully.' : 'The item was updated successfully.';
+
+      $sf_easy_auth_user_base = $form->save();
+
+      $this->dispatcher->notify(new sfEvent($this, 'admin.save_object', array('object' => $sf_easy_auth_user_base)));
+
+      if ($request->hasParameter('_save_and_add'))
+      {
+        $this->getUser()->setFlash('notice', $notice.' You can add another one below.');
+
+        $this->redirect('@sf_easy_auth_user_new');
+      }
+      else
+      {
+        $this->getUser()->setFlash('notice', $notice);
+
+        $this->redirect(array('sf_route' => 'sf_easy_auth_user'));
+      }
+    }
+    else
+    {
+      $this->getUser()->setFlash('error', 'The item has not been saved due to some errors.', false);
+    }
+  }
 }
